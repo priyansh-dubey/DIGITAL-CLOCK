346668de8d299da0c979a225dd0788cdbbfe620a
Priyansh dubey <158035924+priyansh-dubey@users.noreply.github.com>
2024-01-29 15:37:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformat');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    setInterval(showTime,1000);
+});
+
+
+const showTime = ()=>{
+    let date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let secs = date.getSeconds();
+
+    hr = hr<10 ? `0${hr}` : hr;
+    min = min<10 ? `0${min}` : min;
+    secs = secs<10 ? `0${secs}` : secs;
+
+    time.innerHTML =` ${hr} : ${min} : ${secs} ` ;
+
+    timeformat.innerHTML = hr>=12 ? "PM" : "AM" ;
+
+    if((hr>=18) || (hr<6)){
+        document.body.style.backgroundImage = "url('image/sunset.jpg')";
+    }
+}
